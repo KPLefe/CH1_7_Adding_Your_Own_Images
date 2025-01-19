3796c081c2722b9f34a20896ad661862c398d8a2
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-19 17:44:18 -0500

    Use of Asset Catalog. Add custom images. Use Button toggle and ternary to toggle between two images

--- CH1_7_Adding_Your_Own_Images/ContentView.swift
+++ CH1_7_Adding_Your_Own_Images/ContentView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageName = ""
+    @State private var imageTextName = ""
     var body: some View {
+        Spacer()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(imageName)
+                .resizable()
+                .scaledToFit( )
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius:30)
+            Text(imageTextName)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
         }
+        Spacer()
+        HStack {
+            Button("Press Me") {
+                let message1 = "You are awesome"
+                let message2 = "You are Great!"
+                imageTextName = (imageTextName == message1) ? message2 : message1
+                imageName = (imageName == "image0") ? "image1" : "image0"
+                
+                
+            }
+        }
+        
+        .buttonStyle(.borderedProminent)
         .padding()
     }
 }
