1e9af491d8e42ac0c243694894edaad8b7c469b5
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-19 17:30:04 -0500

    Initial Commit

---
+++ CH1_7_Adding_Your_Own_Images/CH1_7_Adding_Your_Own_ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH1_7_Adding_Your_Own_ImagesApp.swift
+//  CH1_7_Adding_Your_Own_Images
+//
+//  Created by Kevin Lefebvre on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CH1_7_Adding_Your_Own_ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH1_7_Adding_Your_Own_Images/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH1_7_Adding_Your_Own_Images
+//
+//  Created by Kevin Lefebvre on 1/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
